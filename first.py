fa2dd598abeecf2f17ebd9ff76445ddac741bd3e
Fizaa2007 <fizaamuhammadfareed@gmail.com>
2024-10-30 08:50:34 -0700

    Add files via upload

---
+++ first.py
@@ -0,0 +1,36 @@
+#values of each no
+num1=15
+num2=5
+num3=20
+  
+sum=num1+num2+num3
+diffrence=num1-num2-num3
+product=num1*num2*num3
+average=sum/3
+
+print(sum)
+print(diffrence)
+print(product)
+print(average)
+
+
+
+#triangle
+base=15
+height=10
+area=21*base*height
+
+print("area of triangel is",area)
+
+#circle
+radius=21
+pi=3.14
+a=pi*radius*radius
+print("area of circle =",a)
+
+#rectangle
+lenght=20
+width=15
+area=lenght*width
+
+print("area of rectangle is",area)
